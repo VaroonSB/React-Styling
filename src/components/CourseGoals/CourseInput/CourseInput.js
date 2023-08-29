fc4bb145baa820877ffc0957889260b5baa4c43d
VaroonSB <varoonsb1311@gmail.com>
2023-08-29 09:08:15 +0530

    dynamix css modules added

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -26,7 +26,11 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValidInput && "invalid"}`}>
+      <div
+        className={`${styles["form-control"]} ${
+          !isValidInput && styles.invalid
+        }`}
+      >
         <label>Course Goal</label>
         <input
           type="text"
